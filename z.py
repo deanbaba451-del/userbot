2b189019aced31fa91a107117af7573d701e38e2
deanbaba451-del <deanbaba451@gmail.com>
2026-03-28 00:54:55 +0300

    Update z.py

--- z.py
+++ z.py
@@ -20,107 +20,44 @@ def run_flask():
 TOKEN = "8685088803:AAGomXnBLdQ-ZRz8Mzssl9dS7d_FlKU6zFY"
 OWNER_ID = 6534222591
 HEDEFLER = {} 
-SALDIRI_DURUMU = False
+AKTIF_GRUPLAR = {} # {chat_id: True/False}
 SPAM_HIZI = 0.5 
 
-# --- DEV HARMANLANMIŞ KÜFÜR ARŞİVİ (2 KAT GÜÇLENDİRİLDİ) ---
+# --- HARMANLANMIŞ DEV KÜFÜR ARŞİVİ ---
 sozler = [
-    "ananın o süzme amcığını ranzayla beraber sikeyim", 
-    "kxrem senin o süzme orospu anneni sike sike dilsiz bıraksın",
-    "emekli babanın o titrek ellerini dölümle yıkayayım",
-    "hasret ananın amına tazyikli su sıkıp silsilece siktirsin",
-    "bacının amcığına beton döküp üzerinde heykelini sikeyim", 
-    "yiğit babanın o kambur sırtında dölüyle resim yapsın",
-    "ananın amına nükleer santral kurup mahallece boşalalım",
-    "şahabettin ananın amına jeneratör sokup tüm sülaleni siktirsin",
-    "seni doğuran o ebenin parmaklarını sike sike koparayım", 
-    "qq ananı pavyon köşelerinde sermaye yapıp kemiklerini kırsın",
-    "ananın amına tır dorsesi sokup park edeyim ucube evladı", 
-    "kxrem bacının o daracık amcığını sike sike patlatsın",
-    "babanın o çürümüş mezarını sike sike deşeyim orospu artığı", 
-    "hasret sülalendeki tüm kadınları ahıra kapatıp sike sike delirsin",
-    "ananın amında saltanat kurar hüküm sürerim",
-    "yiğit o ucube ananı kölem yapıp kırbaçla siktirsin",
-    "regl ananın kanını babana şarap niyetine içireyim ucube", 
-    "şahabettin babanın kasketine boşalıp ananın kafasından aşağı döksün",
-    "bacını kurban pazarında siktire siktire öldüreyim",
-    "qq senin o fakir ve ezik ruhunu sülalenle beraber sikeyim",
-    "ananın amına kepçe sokup kanalizasyon açayım", 
-    "kxrem annenle grup yaparken babana da kamerayı tuttursun",
-    "o ucube sülaleni mezarlıkta sıraya dizip sikeyim",
-    "hasret ananın o süzme amcığına beton mikseriyle girsin",
-    "ananı her gece başka mahallede siktireyim orospu dölü", 
-    "yiğit senin o beyinsiz kafatasını sike sike dağıtsın",
-    "babanın o amele suratına tazyikli boşalayım",
-    "şahabettin ananın amına elektrik direği dikip trafo gibi kullansın",
-    "ananın amına asansör kurup kat kat sikeyim ucube evladı", 
-    "qq senin o ucube gen haritanı sikeyim orospu artığı",
-    "senin varlığını galaksiden kazıyıp sikeyim",
-    "kxrem ananın amına asansör kurup her katta ayrı siktirsin",
-    "ananın amına foseptik çukuru açıp içine boşalayım", 
-    "hasret babanın mezarındaki toprakları sike sike çamur etsin",
-    "bacının o ucube amcığını testereyle kesip sikeyim",
-    "yiğit bacını otobanda tır şoförlerine meze yapsın",
-    "ananın amında mangal yapıp babana yedireyim orospu çocuğu", 
-    "şahabettin sülaleni mezarlıkta sıraya dizip silsilece siktirsin",
-    "seni doğuran o ananın amcığını sikeyim",
-    "qq ananın o buruşmuş amcığını jiletle kesip tuz bassın",
-    "ananın amına tazyikli su sıkıp temizledikten sonra silsilece siktireyim", 
-    "kxrem senin o ucube babanın kel kafasına döl fışkırtsın",
-    "babanın o kambur sırtında dölümle resim yapayım ucube",
-    "hasret ananı 7 kule zindanlarına atıp sike sike köle etsin",
-    "ananın amına elektrik direği dikip trafo gibi kullanayım", 
-    "yiğit bacının amına bayrak direği sokup dalgalandırsın",
-    "senin o beyinsiz kafatasını sike sike dağıtayım",
-    "şahabettin ananın amına yangın tüpü boşaltıp söndürsün",
-    "ananın amına jeneratör sokup elektrik üreterek tüm sülaleni sikeyim", 
-    "qq senin doğduğun günü sülaleni geçmişini geleceğini sikeyim",
-    # --- YENİ EKLENEN 2. KAT KÜFÜRLER ---
-    "ananın amına inşaat iskelesi kurup 40 ameleyle sikeyim",
-    "kxrem ananın amına matkapla girip delik deşik etsin",
-    "hasret bacını kurban bayramında koç niyetine siktirsin",
-    "yiğit senin o fakir babanın kamburunu dölüyle düzeltsin",
-    "şahabettin ananın amına asansör kurup 10 kişiyle çıksın",
-    "qq senin o köylü sülaleni şehrin ortasında sike sike inletsin",
-    "ananın amına vapur yanaştırıp liman yapayım orospu dölü",
-    "babanın tabutunu sikeyim ucube evladı mezarını sikeyim",
-    "ananın amına kanca atıp denizde sikeyim balıklara yedireyim",
-    "bacını pavyonda şampanya niyetine patlatayım",
-    "kxrem senin o ucube sülaleni her gece kabusa çevirsin",
-    "hasret ananın amına nükleer bomba atıp patlatsın",
-    "yiğit bacını otogarda tır şoförlerine sermaye etsin",
-    "şahabettin ananın o kara amcığını sike sike beyazlatsın",
-    "qq senin o ucube hayatını sikeyim siktir git öl",
-    "ananın amına mikserle girip ayran yapayım babana içireyim",
-    "kxrem senin o ucube genlerini sike sike düzeltsin",
-    "hasret bacının amcığını testereyle biçip silsilece siktirsin",
-    "yiğit ananı her gece başka bir pavyonun önüne bağlasın",
-    "şahabettin sülalendeki tüm kadınları köle pazarına satsın",
-    "qq senin o süzme babanın mezarını dölle sulasın",
-    "ananın amına kalorifer peteği sokup kışın ısınayım",
-    "kxrem bacını sike sike dilsiz bırakıp kölesi yapsın",
-    "hasret ananın amına tır makası sokup her gece inletsin",
-    "yiğit senin o ucube varlığını yerle bir edip silsileni silsin",
-    "şahabettin ananın amına dinamit döşeyip havaya uçursun",
-    "qq senin o fakir ve zavallı onurunu sikeyim",
-    "ananın amına beton mikseriyle girip anıt mezar yapayım",
-    "kxrem sülalendeki her kadını sike sike delirsin",
-    "hasret babanın kel kafasında dölüyle perküsyon çalsın",
-    "yiğit ananın amına tünel açıp içinden metro geçirsin",
-    "şahabettin bacının o dar amcığını sike sike mühürlesin",
-    "qq senin doğduğun hastanenin başhekimini sikeyim",
-    "ananın amına jeneratör sokup mahalleye elektrik vereyim",
-    "kxrem ananın amına tazyikli su sıkıp temizledikten sonra siktirsin",
-    "hasret senin o süzme sülaleni mezarlıkta sıraya dizsin",
-    "yiğit ananın amına beton döküp dondurduktan sonra silsilece siktirsin",
-    "şahabettin babanın mezar taşını sike sike toz etsin",
-    "qq senin o ucube hayatını sikeyim siktir git geber",
-    "ananın amına yangın söndürme tüpü boşaltıp kış uykusuna yatayım",
-    "kxrem sülalendeki tüm bakireleri sıraya dizip siktirsin",
-    "hasret ananın o buruşmuş amcığını jiletle kesip tuz bassın",
-    "yiğit senin o ucube babanın kamburunu dölüyle düzeltsin",
-    "şahabettin ananın amına asansör kurup her katta ayrı siktirsin",
-    "qq senin o fakir sülaleni sike sike zengin edeyim"
+    "ananın o süzme amcığını ranzayla beraber sikeyim", "kxrem senin o süzme orospu anneni sike sike dilsiz bıraksın",
+    "hasret ananın amına tazyikli su sıkıp silsilece siktirsin", "yiğit babanın o kambur sırtında dölüyle resim yapsın",
+    "şahabettin ananın amına jeneratör sokup tüm sülaleni siktirsin", "qq ananı pavyon köşelerinde sermaye yapıp kemiklerini kırsın",
+    "ananın amına tır dorsesi sokup park edeyim ucube evladı", "kxrem bacının o daracık amcığını sike sike patlatsın",
+    "hasret sülalendeki tüm kadınları ahıra kapatıp sike sike delirsin", "yiğit o ucube ananı kölem yapıp kırbaçla siktirsin",
+    "şahabettin babanın kasketine boşalıp ananın kafasından aşağı döksün", "qq senin o fakir ve ezik ruhunu sülalenle beraber sikeyim",
+    "kxrem annenle grup yaparken babana da kamerayı tuttursun", "hasret ananın o süzme amcığına beton mikseriyle girsin",
+    "yiğit senin o beyinsiz kafatasını sike sike dağıtsın", "şahabettin ananın amına elektrik direği dikip trafo gibi kullansın",
+    "qq senin o ucube gen haritanı sikeyim orospu artığı", "kxrem ananın amına asansör kurup her katta ayrı siktirsin",
+    "hasret babanın mezarındaki toprakları sike sike çamur etsin", "yiğit bacını otobanda tır şoförlerine meze yapsın",
+    "şahabettin sülaleni mezarlıkta sıraya dizip silsilece siktirsin", "qq ananın o buruşmuş amcığını jiletle kesip tuz bassın",
+    "kxrem senin o ucube babanın kel kafasına döl fışkırtsın", "hasret ananı 7 kule zindanlarına atıp sike sike köle etsin",
+    "yiğit bacının amına bayrak direği sokup dalgalandırsın", "şahabettin ananın amına yangın tüpü boşaltıp söndürsün",
+    "qq senin doğduğun günü sülaleni geçmişini geleceğini sikeyim", "ananın amına inşaat iskelesi kurup 40 ameleyle sikeyim",
+    "kxrem ananın amına matkapla girip delik deşik etsin", "hasret bacını kurban bayramında koç niyetine siktirsin",
+    "yiğit senin o fakir babanın kamburunu dölüyle düzeltsin", "şahabettin ananın amına asansör kurup 10 kişiyle çıksın",
+    "qq senin o köylü sülaleni şehrin ortasında sike sike inletsin", "ananın amına vapur yanaştırıp liman yapayım orospu dölü",
+    "babanın tabutunu sikeyim ucube evladı mezarını sikeyim", "ananın amına kanca atıp denizde sikeyim balıklara yedireyim",
+    "bacını pavyonda şampanya niyetine patlatayım", "kxrem senin o ucube sülaleni her gece kabusa çevirsin",
+    "hasret ananın amına nükleer bomba atıp patlatsın", "yiğit bacını otogarda tır şoförlerine sermaye etsin",
+    "şahabettin ananın o kara amcığını sike sike beyazlatsın", "qq senin o ucube hayatını sikeyim siktir git geber",
+    "ananın amına mikserle girip ayran yapayım babana içireyim", "kxrem senin o ucube genlerini sike sike düzeltsin",
+    "hasret bacının amcığını testereyle biçip silsilece siktirsin", "yiğit ananı her gece başka bir pavyonun önüne bağlasın",
+    "şahabettin sülalendeki tüm kadınları köle pazarına satsın", "qq senin o süzme babanın mezarını dölle sulasın",
+    "ananın amına kalorifer peteği sokup kışın ısınayım", "kxrem bacını sike sike dilsiz bırakıp kölesi yapsın",
+    "hasret ananın amına tır makası sokup her gece inletsin", "yiğit senin o ucube varlığını yerle bir edip silsileni silsin",
+    "şahabettin ananın amına dinamit döşeyip havaya uçursun", "qq senin o fakir ve zavallı onurunu sikeyim",
+    "ananın amına beton mikseriyle girip anıt mezar yapayım", "kxrem sülalendeki her kadını sike sike delirsin",
+    "hasret babanın kel kafasında dölüyle perküsyon çalsın", "yiğit ananın amına tünel açıp içinden metro geçirsin",
+    "şahabettin bacının o dar amcığını sike sike mühürlesin", "qq senin doğduğun hastanenin başhekimini sikeyim",
+    "ananın amına jeneratör sokup mahalleye elektrik vereyim", "kxrem sülalendeki tüm bakireleri sıraya dizip siktirsin",
+    "hasret ananın o buruşmuş amcığını jiletle kesip tuz bassın", "yiğit senin o ucube babanın kamburunu dölüyle düzeltsin",
+    "şahabettin ananın amına asansör kurup her katta ayrı siktirsin", "qq senin o fakir sülaleni sike sike zengin edeyim"
 ]
 
 def anti_spam_text(text):
@@ -137,8 +74,8 @@ async def monitor_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         HEDEFLER[key]["name"] = update.message.from_user.first_name
 
 async def ekle_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id != OWNER_ID or not context.args: return
-    target = context.args[0]
+    if update.effective_user.id != OWNER_ID: return
+    target = context.args[0] if context.args else "Hedef"
     HEDEFLER[target] = {"last_msg_id": None, "name": target}
     await context.bot.send_message(chat_id=OWNER_ID, text=f"🎯 Hedef eklendi: {target}")
 
@@ -146,21 +83,23 @@ async def bulk_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id != OWNER_ID: return
     for t in context.args:
         HEDEFLER[t] = {"last_msg_id": None, "name": t}
-    await context.bot.send_message(chat_id=OWNER_ID, text=f"💣 {len(context.args)} hedef listeye alındı.")
+    await context.bot.send_message(chat_id=OWNER_ID, text=f"💣 {len(context.args)} kişi listeye alındı.")
 
 async def baslat_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global SALDIRI_DURUMU
-    if update.effective_user.id != OWNER_ID or not context.args: return
-    grup_id = context.args[0]
-    SALDIRI_DURUMU = True
-    await context.bot.send_message(chat_id=OWNER_ID, text=f"🚀 {grup_id} üzerinde imha başladı.")
+    if update.effective_user.id != OWNER_ID: return
+    # Eğer argüman varsa (kullanıcı adı), onu kullan; yoksa içinde bulunulan grubu kullan
+    grup_id = context.args[0] if context.args else update.effective_chat.id
     
-    while SALDIRI_DURUMU:
+    AKTIF_GRUPLAR[str(grup_id)] = True
+    await context.bot.send_message(chat_id=OWNER_ID, text=f"🚀 {grup_id} grubunda AV BAŞLADI.")
+    
+    while AKTIF_GRUPLAR.get(str(grup_id), False):
         for t_id, data in list(HEDEFLER.items()):
-            if not SALDIRI_DURUMU: break
+            if not AKTIF_GRUPLAR.get(str(grup_id), False): break
             try:
                 soz = anti_spam_text(random.choice(sozler))
                 msg_text = f"**{data['name'].upper()}** {soz}"
+                
                 if data["last_msg_id"]:
                     await context.bot.send_message(chat_id=grup_id, text=msg_text, reply_to_message_id=data["last_msg_id"], parse_mode="Markdown")
                 else:
@@ -169,18 +108,28 @@ async def baslat_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except:
                 await asyncio.sleep(1)
 
+async def dur_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != OWNER_ID: return
+    
+    # 1. Senaryo: DM'den /dur @grupismi yazmak
+    if context.args:
+        target_chat = context.args[0]
+        AKTIF_GRUPLAR[str(target_chat)] = False
+        await context.bot.send_message(chat_id=OWNER_ID, text=f"🛑 {target_chat} durduruldu.")
+    
+    # 2. Senaryo: Grubun içinde direkt /dur yazmak (Özellikle gizli gruplar için)
+    else:
+        current_chat = str(update.effective_chat.id)
+        if current_chat in AKTIF_GRUPLAR:
+            AKTIF_GRUPLAR[current_chat] = False
+            # Grup içinde iz bırakmamak için onay mesajını yine DM'den atar
+            await context.bot.send_message(chat_id=OWNER_ID, text=f"🛑 Gizli gruptaki ({current_chat}) saldırı sonlandırıldı.")
+
 async def sleep_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global SPAM_HIZI
     if update.effective_user.id == OWNER_ID and context.args:
         SPAM_HIZI = float(context.args[0])
-        await context.bot.send_message(chat_id=OWNER_ID, text=f"⏱ Yeni hız: {SPAM_HIZI}s")
-
-async def dur_komutu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    global SALDIRI_DURUMU
-    if update.effective_user.id == OWNER_ID:
-        SALDIRI_DURUMU = False
-        HEDEFLER.clear()
-        await context.bot.send_message(chat_id=OWNER_ID, text="🛑 OPERASYON DURDURULDU.")
+        await context.bot.send_message(chat_id=OWNER_ID, text=f"⏱ Hız: {SPAM_HIZI}s")
 
 if __name__ == '__main__':
     threading.Thread(target=run_flask, daemon=True).start()
