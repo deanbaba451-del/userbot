6af07abad15b82bb2313f07e1d586deda61d8a46
deanbaba451-del <deanbaba451@gmail.com>
2026-03-27 19:42:00 +0300

    Update z.py

--- z.py
+++ z.py
@@ -6,7 +6,6 @@ from flask import Flask
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters, CommandHandler
 
-# render.com ayarı
 app = Flask('')
 
 @app.route('/')
@@ -20,57 +19,71 @@ def run_flask():
 # --- AYARLAR ---
 TOKEN = "8685088803:AAGomXnBLdQ-ZRz8Mzssl9dS7d_FlKU6zFY"
 OWNER_ID = 6534222591
-HEDEFLER = set()
-AKTIF_MESAJLAR = {} # Her user_id için en son mesaj id'sini tutar
+HEDEFLER = {} 
+AKTIF_MESAJLAR = {} 
+SALDIRI_DURUMU = False
+SPAM_HIZI = 2 
 
 sozler = [
-    "siktir git öl", "ananın amcığını sikiyim", "emekli babanın kel kafasına döllerimi dökeyim",
-    "şahabettin anneni siksin", "orospunhn çocuğu seni", "yaz yaz", "o amele babanın kamburunu sikerim",
-    "salak ucube orospu çocuğu", "o ananın amcığındaki yumurtaları yerim", "amına doğumun oğlu",
-    "qq anneni siksin", "annen şahabbetine amcığını yedirdi", "ölmüşlerinin mezarına çiçek yerine dildo koyarım",
-    "regl ananın kanını babana içiririm", "otobanda otostop çeken bacına 250km hızla çarparım",
-    "o ananın amına koç taşşağı fırlatırım", "ananı 7 kule zindanlarına atar sike sike kölem ederim",
-    "o ananın sırtına binip at gibi kişnete kişnete sikeyim", "annenin göt deliğini parmaklarım sonra boklu parmagımı kendi götüme sokarım",
-    "o ananın amında saltanat kurar hüküm sürerim orospu çocuğu", "ananın ölüsünü sikeyim", "babanın mezarını sikeyim",
-    "bacının amcığına beton dökeyim", "o ananın amına tır sokayım", "soyunun sopunun amına koyayım",
-    "senin tipini sikeyim ucube", "ananın amına kireç dökerim", "babanın sakalını sikeyim", "bacını pazarda satayım",
-    "o ananın amına kaktüs sokayım", "senin ben doğduğun günü sikeyim", "ananın amına tekmeyle gireyim",
-    "babanın kemiklerini sikeyim", "orospu evladı seni", "ananın amını parçalarım", "seni sike sike öldürürüm",
-    "babanın kafa tasını sikeyim", "ananın götüne kazık çakayım", "bacının amına dildo sokayım",
-    "senin o ucube sıfatını sikeyim", "ananın amına uçak indiririm", "babanın bıyığını sikeyim",
-    "senin sülaleni sikeyim", "ananın amına dinamit lokumu döşeyim", "bacının amını deşeyim",
-    "senin ben karakterini sikeyim", "ananın amına hortum sokayım", "babanın mezarındaki toprakları sikeyim",
-    "o ananın amını darmadağın ederim", "ananın amına soba borusu sokup ısınayım", "senin o sülalendeki bütün kadınları üst üste koyup sikeyim",
-    "babanın tabutunu sikeyim orospu dölü", "ananın amına ranzayla girerim", "senin o ezik genlerini sikeyim",
-    "bacının amına bayrak direği sokayım", "ananın göt deliğine asit dökeyim", "senin ben cibiliyetini sikeyim",
-    "babanın mezarındaki çiçekleri sikeyim", "ananı her gece başka mahallede siktireyim", "senin o süzme orospu anneni sikeyim",
-    "ananın amına çapa atayım", "bacının amcığını testereyle keseyim", "senin varlığını sikeyim ucube evladı",
-    "babanın kamburundaki her kemiği sikeyim", "ananın amına vapur yanaştırayım", "seni doğurtan ebenin parmaklarını sikeyim",
-    "ananın amında mangal yapayım", "senin sülalendeki ölüleri diriltip sikeyim", "ananın götüne şemsiye sokup açayım",
-    "babanın ölüsünü sikeyim", "ananın amına beton mikseri sokayım", "senin o fakir fukara sülaleni sikeyim",
-    "ananı sike sike komaya sokayım", "bacının amına matkapla gireyim", "senin o silik karakterini sikeyim",
-    "ananın amına nükleer bomba atayım", "babanın mezar taşını sikeyim", "ananın amına yangın tüpü boşaltayım",
-    "seni doğuran o ananın amcığını sikeyim", "ananın amına elektrik direği sokayım", "senin o köylü babanın kasketini sikeyim",
-    "ananın amına jeneratör sokayım", "bacının götünü sike sike kanatayım", "senin sülaleni mezarlıkta sikeyim",
-    "ananın amına tazyikli su sıkayım", "babanın o amele suratını sikeyim", "ananı pavyon köşelerinde sikeyim",
-    "senin o ucube varlığını ortadan kaldırayım", "ananın amına asansör kurayım", "bacının amcığını sike sike çürüteyim",
-    "senin o köhne sülaleni sikeyim", "ananın amına foseptik çukuru açayım", "babanın o kambur sırtını sikeyim",
-    "ananı sike sike öldürüp mezara gömeyim", "senin o beyinsiz kafanı sikeyim", "ananın amına tır dorsesi sokayım",
-    "bacını sike sike dilsiz bırakayım", "senin o ucube gen haritanı sikeyim", "ananın amına tünel açayım",
-    "babanın o ölü gözlerini sikeyim", "ananı sike sike sokağa atayım", "senin o zavallı hayatını sikeyim",
-    "ananın amına kepçe sokayım", "bacının o ucube amcığını sikeyim", "senin o fakir ruhunu sikeyim",
-    "ananın amına kanca atayım", "babanın o çürümüş dişlerini sikeyim", "ananı sike sike delirteyim",
-    "senin o ucube suratına dölümü boşaltayım", "ananın amına kalorifer kazanı sokayım", "bacının o amcığını sike sike patlatayım",
-    "senin sülalendeki her kadını sikeyim", "ananın amına matkapla gireyim", "babanın o ucube mezarını sikeyim"
+    "siktir git çık gruptan ananı sikeyim", "o ucube tipinle bu grupta ne işin var anasını siktiğim",
+    "çık gruptan yoksa ananın amcığına beton dökerim", "gruptan çık ucube dölü ananı sike sike öldürürüm",
+    "senin bu grupta nefes alışını sikeyim ucube evladı", "ananın amına ranzayla girerim gruptan siktir git",
+    "o amele babanın kamburunu sikeyim çık şu gruptan", "senin o ucube varlığını gruptan kazırım",
+    "gruptan çıkmazsan ananın amına nükleer bomba atarım", "siktir git öl ucube evladı", 
+    "ananın o süzme amcığını sikiyim", "emekli babanın kel kafasına döllerimi dökeyim",
+    "orospunun öz evladı seni", "ananın amcığındaki yumurtaları tek tek yutayım",
+    "amına doğumun ucube dölü", "qq annenle grup yapsın orospu evladı", 
+    "ölmüşlerinin mezarına çiçek yerine dildo dikeyim", "regl ananın kanını babana şarap niyetine içireyim", 
+    "otobanda otostop çeken bacına tırla çarpıp sikeyim", "o ananın amına koç taşşağı fırlatayım", 
+    "ananı 7 kule zindanlarında sike sike kölem edeyim", "o ananın sırtına binip kırbaçla sikeyim", 
+    "ananın amında saltanat kurar hüküm sürerim", "babanın mezarındaki toprakları sike sike çamur edeyim", 
+    "bacının amcığına beton döküp heykelini sikeyim", "ananın amına tır dorsesi sokup park edeyim", 
+    "senin doğduğun hastaneyi sülaleni sikeyim", "babanın tabutunu sikeyim orospu artığı", 
+    "bacının amına bayrak direği sokup dalgalandırayım", "senin o ucube gen haritanı sikeyim",
+    "ananın amına jeneratör sokup elektrik üreteyim", "babanın o çürümüş dişlerini sike sike dökeyim",
+    "ananın amına kalorifer kazanı sokup ısıtayım", "bacının o ucube amcığını sike sike çürüteyim",
+    "senin o fakir ruhunu sülaleni sikeyim", "ananın amına kanca atıp denizde sikeyim",
+    "babanın kambur sırtında dölümle resim yapayım", "ananı sike sike öldürüp mezarını sikeyim",
+    "senin o beyinsiz kafatasını sikeyim", "bacını sike sike dilsiz bırakıp kölem yapayım",
+    "ananın amına asansör kurup inip çıkayım", "senin o ucube varlığını galaksiden sileyim",
+    "ananın amına foseptik çukuru açıp içine sikeyim", "babanın mezar taşını sikeyim ucube dölü",
+    "ananın amına yangın tüpü boşaltıp söndüreyim", "seni doğuran o ananın amcığını sikeyim",
+    "ananın amına elektrik direği sokup trafo yapayım", "senin o köylü babanın kasketini sikeyim",
+    "ananın amına jeneratör sokup mahallece sikelim", "bacının götünü sike sike kanatıp içeyim",
+    "senin sülaleni mezarlıkta sıraya dizip sikeyim", "ananın amına tazyikli su sıkıp temizleyeyim",
+    "babanın o amele suratına boşalayım", "ananı pavyon köşelerinde siktire siktire öldüreyim",
+    "senin o ucube varlığını yerle bir edeyim", "ananın amına asansör kurup kat kat sikeyim",
+    "bacının amcığını sike sike patlatayım", "senin o köhne sülaleni sikeyim",
+    "ananın amına kepçe sokup kanalizasyon yapayım", "babanın o kambur belini sikeyim",
+    "senin o ucube suratına dölümü fışkırtayım", "ananın amına tünel açıp içinden geçeyim",
+    "babanın o ölü gözlerini sikeyim", "ananı sike sike sokağa atıp rezil edeyim",
+    "senin o zavallı hayatını sülaleni sikeyim", "ananın amına matkapla girip delik deşik edeyim",
+    "bacının o ucube amcığını sikeyim", "senin o fakir sülaleni sikeyim",
+    "ananın amına kireç döküp yakayım", "babanın mezarındaki çiçekleri sikeyim",
+    "ananı her gece başka mahallede siktireyim", "senin o süzme orospu anneni sikeyim",
+    "ananın amına çapa atayım", "bacının amcığını testereyle kesip sikeyim",
+    "senin varlığını sikeyim ucube evladı", "babanın kamburundaki her kemiği tek tek sikeyim",
+    "ananın amına vapur yanaştırıp liman yapayım", "seni doğuran ebenin parmaklarını sikeyim",
+    "ananın amında mangal yapıp babana yedireyim", "senin sülalendeki ölüleri diriltip sikeyim",
+    "ananın götüne şemsiye sokup içinde açayım", "babanın o ölü suratını sikeyim",
+    "ananın amına beton mikseri sokup dondurayım", "ananın amına tır makası sokup sikeyim",
+    "bacını kurban pazarında siktireyim", "senin o ucube soyunu sopunu kurutayım",
+    "ananın amına dinamit döşeyip patlatayım", "babanın o çürük mezarını sike sike deşeyim",
+    "ananı sike sike gökyüzüne fırlatayım", "senin o süzme ucube canını sikeyim",
+    "ananın amına kalorifer peteği sokup ısıtayım", "bacının o ucube amını sike sike patlatayım",
+    "ananın o amcığını sike sike mühürleyeyim", "senin o ucube babanın mezarındaki toprakları sikeyim",
+    "ananın amına nükleer santral kurayım", "senin sülalendeki her kadını sike sike delirteyim"
 ]
 
 async def spam_dongusu(context, chat_id, user_id, msg_id):
-    # Bu döngü sadece mesaj "güncel" ise devam eder
-    while user_id in HEDEFLER and AKTIF_MESAJLAR.get(user_id) == msg_id:
+    isim = HEDEFLER.get(user_id, "")
+    prefix = f"{isim.lower()} " if isim else ""
+    
+    while SALDIRI_DURUMU and user_id in HEDEFLER and AKTIF_MESAJLAR.get(user_id) == msg_id:
         try:
             cevap = random.choice(sozler)
-            await context.bot.send_message(chat_id=chat_id, text=cevap, reply_to_message_id=msg_id)
-            await asyncio.sleep(2)
+            await context.bot.send_message(chat_id=chat_id, text=f"{prefix}{cevap}", reply_to_message_id=msg_id)
+            await asyncio.sleep(SPAM_HIZI)
         except:
             break
 
@@ -79,15 +92,17 @@ async def target_ekle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         HEDEFLER.clear()
         AKTIF_MESAJLAR.clear()
         try:
-            target_id = None
+            target_user = None
             if update.message.reply_to_message:
-                target_id = update.message.reply_to_message.from_user.id
+                target_user = update.message.reply_to_message.from_user
             elif context.args:
-                target_id = int(context.args[0].strip())
+                uid = int(context.args[0].strip())
+                target_user = (await context.bot.get_chat_member(update.effective_chat.id, uid)).user
             
-            if target_id:
-                HEDEFLER.add(target_id)
-                await update.message.reply_text(f"{target_id} hedef yapıldı")
+            if target_user:
+                name = target_user.first_name if target_user.first_name else ""
+                HEDEFLER[target_user.id] = name
+                await update.message.reply_text(f"{name if name else 'isim yok'} hedef yapıldı")
         except: pass
 
 async def bulk_ekle(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -97,25 +112,43 @@ async def bulk_ekle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         raw_ids = " ".join(context.args).replace("@", "").split(",")
         for r_id in raw_ids:
             if r_id.strip().isdigit():
-                HEDEFLER.add(int(r_id.strip()))
+                uid = int(r_id.strip())
+                try:
+                    user_info = await context.bot.get_chat_member(update.effective_chat.id, uid)
+                    name = user_info.user.first_name if user_info.user.first_name else ""
+                    HEDEFLER[uid] = name
+                except:
+                    HEDEFLER[uid] = ""
         await update.message.reply_text(f"{len(HEDEFLER)} kişi listeye eklendi")
 
 async def tetikleyici(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global SALDIRI_DURUMU, SPAM_HIZI
     user_id = update.effective_user.id
     chat_id = update.effective_chat.id
-    msg_id = update.message.message_id
-    text = update.message.text
+    msg_id = update.message.id
+    text = update.message.text or ""
+
+    if text.lower().startswith("/hasret") and user_id == OWNER_ID:
+        SALDIRI_DURUMU = True
+        try:
+            args = text.split()
+            if len(args) > 1: SPAM_HIZI = float(args[1])
+        except: SPAM_HIZI = 2
+        await context.bot.send_message(chat_id=chat_id, text=f"başladı (hız: {SPAM_HIZI}s)", reply_to_message_id=msg_id)
+        return
 
     if text.lower() == "/yigit" and user_id == OWNER_ID:
+        SALDIRI_DURUMU = False
         HEDEFLER.clear()
         AKTIF_MESAJLAR.clear()
         await context.bot.send_message(chat_id=chat_id, text="tmm", reply_to_message_id=msg_id)
         return
 
-    if user_id in HEDEFLER:
-        # En son mesaj id'sini güncelle (bot artık bu mesaj üzerinden devam edecek)
+    if SALDIRI_DURUMU and user_id in HEDEFLER:
+        try:
+            await context.bot.send_message(chat_id=OWNER_ID, text="sikme hazır")
+        except: pass
         AKTIF_MESAJLAR[user_id] = msg_id
-        # Paralel görev başlat (diğer hedefleri engellemez)
         asyncio.create_task(spam_dongusu(context, chat_id, user_id, msg_id))
 
 if __name__ == '__main__':
